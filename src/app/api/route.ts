5572b6d75bdf343a21616cd5227725de0489f0e5
Agboola Boluwatife <ceeprel3@gmail.com>
2024-09-01 23:09:52 +0100

    Updated

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,49 +1,49 @@
-// // /pages/api/fileUpload.ts
+// /pages/api/fileUpload.ts
 
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import fs from "fs";
-// import path from "path";
+import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
 
-// export default function fileUpload(req: NextApiRequest, res: NextApiResponse) {
-//   if (req.method === "POST") {
-//     try {
-//       const { id, fileName, file } = req.body;
+export default function fileUpload(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    try {
+      const { id, fileName, file } = req.body;
 
-//       // Validate incoming data
-//       if (!id || !fileName || !file) {
-//         return res
-//           .status(400)
-//           .json({ success: false, message: "Missing required fields." });
-//       }
+      // Validate incoming data
+      if (!id || !fileName || !file) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Missing required fields." });
+      }
 
-//       const fileExtension = fileName.split(".").pop();
-//       if (!fileExtension) {
-//         return res
-//           .status(400)
-//           .json({ success: false, message: "Invalid file name." });
-//       }
+      const fileExtension = fileName.split(".").pop();
+      if (!fileExtension) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Invalid file name." });
+      }
 
-//       const imagesDir = path.join(process.cwd(), "public", "images");
-//       if (!fs.existsSync(imagesDir)) {
-//         fs.mkdirSync(imagesDir, { recursive: true });
-//       }
+      const imagesDir = path.join(process.cwd(), "public", "images");
+      if (!fs.existsSync(imagesDir)) {
+        fs.mkdirSync(imagesDir, { recursive: true });
+      }
 
-//       const filePath = path.join(imagesDir, `${id}.${fileExtension}`);
+      const filePath = path.join(imagesDir, `${id}.${fileExtension}`);
 
-//       console.log(filePath);
+      console.log(filePath);
 
-//       const imageBuffer = Buffer.from(file, "base64");
+      const imageBuffer = Buffer.from(file, "base64");
 
-//       fs.writeFileSync(filePath, imageBuffer);
+      fs.writeFileSync(filePath, imageBuffer);
 
-//       res
-//         .status(200)
-//         .json({ success: true, path: `/images/${id}.${fileExtension}` });
-//     } catch (error) {
-//       console.error("Error saving image:", error);
-//       res.status(500).json({ success: false, message: "Error saving image" });
-//     }
-//   } else {
-//     res.status(405).json({ message: "Method not allowed" });
-//   }
-// }
+      res
+        .status(200)
+        .json({ success: true, path: `/images/${id}.${fileExtension}` });
+    } catch (error) {
+      console.error("Error saving image:", error);
+      res.status(500).json({ success: false, message: "Error saving image" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
