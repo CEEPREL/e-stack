ffbddaa67f8dde9c3b1db7d5a7ead11f43f2395c
CEEPREL <ceeprel3@gmail.com>
2024-12-01 23:56:44 +0100

    update

--- src/components/payments/flutterWave.tsx
+++ src/components/payments/flutterWave.tsx
@@ -4,7 +4,7 @@ function flutterWave() {
   return (
     <div>
       this is how to pay ehs we want to di with flutter wave anf i am ready to
-      teach every one reafy!!!
+      teach every one reafy!!
     </div>
   );
 }
