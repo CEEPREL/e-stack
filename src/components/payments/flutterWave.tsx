f2b0906ac686869be78e7703412410d16e98c2c9
CEEPREL <ceeprel3@gmail.com>
2024-12-01 23:50:42 +0100

    mailer

---
+++ src/components/payments/flutterWave.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function flutterWave() {
+  return (
+    <div>
+      this is how to pay ehs we want to di with flutter wave anf i am ready to
+      teach every one reafy!!!
+    </div>
+  );
+}
+
+export default flutterWave;
